3f86a7164b38a452b34a31c9b685f7f0b543e4d8
marsidave9697 <marsimoydawit14@gmail.com>
2025-02-12 22:55:13 +0300

    Create ColorfulStones(simplified edition).py

---
+++ ColorfulStones(simplified edition).py
@@ -0,0 +1,10 @@
+s = input().strip()
+t = input().strip()
+
+position = 1
+
+for instruction in t:
+    if position <= len(s) and s[position - 1] == instruction:
+        position += 1
+
+print(position)
